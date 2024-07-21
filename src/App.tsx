568575fb74efe1647971c1691e12c1f79825de4d
timdecillis <timdecillis@gmail.com>
2024-07-21 14:13:15 -0700

    added useState and endpoints state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  const [endpoints, setEndpoints] = useState([]);
+  return <div className="App"></div>;
 }
 
 export default App;
