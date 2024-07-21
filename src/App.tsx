96291c296b71c07da1af596186ed545abc907702
timdecillis <timdecillis@gmail.com>
2024-07-21 14:20:08 -0700

    mapped endpoints

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,14 @@ import "./App.css";
 
 function App() {
   const [endpoints, setEndpoints] = useState([]);
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Endpoints Data</h1>
+      {endpoints.map((data) => (
+        <div>{data}</div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
