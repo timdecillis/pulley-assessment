9a8903b4c1c794d82e1ece15a2c1309e8a4165c8
timdecillis <timdecillis@gmail.com>
2024-07-22 14:57:20 -0700

    finished up to response 3

--- src/App.tsx
+++ src/App.tsx
@@ -13,16 +13,36 @@ function App() {
       hasFetched.current = true;
 
       const response = await axios.get("/api/timdecillis@gmail.com");
-      setObjects((prevData) => [...prevData, response.data]);
+      console.log("1:", response.data.encrypted_path);
+      const response2 = await axios.get(response.data.encrypted_path);
+      console.log("2:", response2.data);
+      let nextPath;
+      function decodeAsciiArray(asciiArray: number[]) {
+        return asciiArray.map((value) => String.fromCharCode(value)).join("");
+      }
+
+      if (
+        response2.data.encryption_method ===
+        "converted to a JSON array of ASCII values"
+      ) {
+        const slicedPath = response2.data.encrypted_path.slice(5);
+        nextPath = decodeAsciiArray(
+          JSON.parse(response2.data.encrypted_path.slice(5))
+        );
+      }
+      const response3 = nextPath ? await axios.get(`task_${nextPath}`) : null;
+      console.log('3:', response3.data)
+
+      // setObjects((prevData) => [...prevData, response.data]);
     };
     fetchEndpointsData();
   }, []);
   return (
     <div className="App">
       <h1>Endpoints Data</h1>
-      {objects.map((object) => {
+      {/* {objects.map((object) => {
         return <div>{object.step}</div>;
-      })}
+      })} */}
     </div>
   );
 }
