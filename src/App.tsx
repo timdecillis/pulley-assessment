e9689352ef17953ac1d439c92d4a418636c86824
timdecillis <timdecillis@gmail.com>
2024-07-22 14:39:24 -0700

    FE revamp

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,25 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
-  const [endpoints, setEndpoints] = useState<any[]>([]);
+  const [objects, setObjects] = useState<any[]>([]);
+
   const hasFetched = useRef(false);
 
   useEffect(() => {
     const fetchEndpointsData = async () => {
       if (hasFetched.current) return;
       hasFetched.current = true;
+
       const response = await axios.get("/api/timdecillis@gmail.com");
-      setEndpoints((prevEndpoints) => [...prevEndpoints, response.data]);
+      setObjects((prevData) => [...prevData, response.data]);
     };
     fetchEndpointsData();
   }, []);
   return (
     <div className="App">
       <h1>Endpoints Data</h1>
-      {endpoints.map((object, i) => {
-        const keys = Object.keys(object);
-        return (
-          <div key={i}>
-            <h5>Step {object.level}</h5>
-            {keys.map((key, index) => {
-              return <div key={index}>{object[key]}</div>;
-            })}
-          </div>
-        );
-
-        return <div>{object.challenger}</div>;
+      {objects.map((object) => {
+        return <div>{object.step}</div>;
       })}
     </div>
   );
