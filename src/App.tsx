b50cdb3398554af22b252eb0d33fcd047a47cfda
timdecillis <timdecillis@gmail.com>
2024-07-23 00:28:33 -0700

    completed refactored queue version up to response 4

--- src/App.tsx
+++ src/App.tsx
@@ -4,58 +4,77 @@ import axios from "axios";
 
 function App() {
   const [objects, setObjects] = useState<any[]>([]);
-
   const hasFetched = useRef(false);
 
-  useEffect(() => {
-    const fetchEndpointsData = async () => {
-      if (hasFetched.current) return;
-      hasFetched.current = true;
+  const swapPairs = (str: string) => {
+    let swappedStr = "";
 
-      const response = await axios.get("/api/timdecillis@gmail.com");
-      console.log("1:", response.data.encrypted_path);
-      const response2 = await axios.get(response.data.encrypted_path);
-      console.log("2:", response2.data);
-      let nextPath;
-      function decodeAsciiArray(asciiArray: number[]) {
-        return asciiArray.map((value) => String.fromCharCode(value)).join("");
+    for (let i = 0; i < str.length; i += 2) {
+      if (i + 1 < str.length) {
+        swappedStr += str[i + 1] + str[i];
+      } else {
+        swappedStr += str[i];
       }
+    }
 
-      if (
-        response2.data.encryption_method ===
-        "converted to a JSON array of ASCII values"
-      ) {
-        const slicedPath = response2.data.encrypted_path.slice(5);
-        nextPath = decodeAsciiArray(
-          JSON.parse(response2.data.encrypted_path.slice(5))
-        );
-      }
-      const response3 = nextPath ? await axios.get(`task_${nextPath}`) : null;
-      let nextPath2;
-      function swapPairs(str: string) {
-        let swappedStr = "";
+    return swappedStr;
+  };
+  const decodeAsciiString = (encryptedStr: string, number: number) => {
+    let decodedStr = "";
 
-        for (let i = 0; i < str.length; i += 2) {
-          if (i + 1 < str.length) {
-            // Swap the pair of characters
-            swappedStr += str[i + 1] + str[i];
-          } else {
-            // If there is an odd number of characters, just add the last character
-            swappedStr += str[i];
-          }
+    for (let i = 0; i < encryptedStr.length; i++) {
+      const decodedCharCode = encryptedStr.charCodeAt(i) + number;
+      decodedStr += String.fromCharCode(decodedCharCode);
+    }
+
+    return decodedStr;
+  };
+  const decodeAsciiArray = (asciiArray: number[]) => {
+    return asciiArray.map((value) => String.fromCharCode(value)).join("");
+  };
+
+  const fetchData = async () => {
+    if (hasFetched.current) return;
+    hasFetched.current = true;
+    let queue = ["/api/timdecillis@gmail.com"];
+    while (queue.length) {
+      const url = queue.pop();
+      if (url) {
+        const response = await axios.get(url);
+        console.log("response:", response.data);
+        let { encrypted_path } = response.data;
+        encrypted_path = encrypted_path.slice(5);
+        if (
+          response.data.encryption_method ===
+          "converted to a JSON array of ASCII values"
+        ) {
+          encrypted_path = decodeAsciiArray(JSON.parse(encrypted_path));
         }
-
-        return swappedStr;
+        if (
+          response.data.encryption_method === "swapped every pair of characters"
+        ) {
+          encrypted_path = swapPairs(encrypted_path);
+        }
+        if (response.data.encryption_method.slice(0, 5) === "added") {
+          let numberToAdd: number;
+          const method = response.data.encryption_method;
+          if (method.slice(7, 8) !== " ") {
+            numberToAdd = method.slice(6, 8);
+          } else {
+            numberToAdd = method.slice(6, 7);
+          }
+          encrypted_path = decodeAsciiString(
+            JSON.parse(encrypted_path),
+            numberToAdd
+          );
+        }
+        queue.push(`task_${encrypted_path}`);
       }
+    }
+  };
 
-      nextPath2 = swapPairs(response3?.data.encrypted_path.slice(5));
-      console.log("3:", nextPath2);
-      const response4 = nextPath2 ? await axios.get(`task_${nextPath2}`) : null;
-      console.log('4:', response4?.data)
-
-      // setObjects((prevData) => [...prevData, response.data]);
-    };
-    fetchEndpointsData();
+  useEffect(() => {
+    fetchData();
   }, []);
   return (
     <div className="App">
