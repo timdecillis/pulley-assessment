142d4cc0f0ca25a255ddad04aaed0dbc11355b06
timdecillis <timdecillis@gmail.com>
2024-07-26 14:44:59 -0700

    completed methods file

--- src/methods.ts
+++ src/methods.ts
@@ -1,12 +1,21 @@
-import { decodeCustomHex, swapPairs } from "./utils";
+import {
+  decodeCustomHex,
+  swapPairs,
+  decodeAsciiArray,
+  decodeAsciiString,
+  decodeBase64Path,
+  decodeScrambledHex,
+  decodeRotated,
+} from "./utils";
 
 export const methods = {
-  "none": (path: string) => path,
-  "converted to a JSON array of ASCII values": (path: string) => path,
+  none: (path: string) => path,
+  "converted to a JSON array of ASCII values": decodeAsciiArray,
   "swapped every pair of characters": swapPairs,
   "encoded it with custom hex character": decodeCustomHex,
-  "scrambled!": (path: string) => path,
-  "encoded as base64": (path: string) => path,
-  "circularly rotated": (path: string) => path,
-  "hashed": (path: string) => path
+  "scrambled!": decodeScrambledHex,
+  "encoded as base64": decodeBase64Path,
+  "circularly rotated": decodeRotated,
+  added: decodeAsciiString,
+  hashed: (path: string) => path,
 };
