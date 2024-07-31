496d163ef691ee8f1cfe9178b8b9c80df40f3045
timdecillis <timdecillis@gmail.com>
2024-07-31 13:02:33 -0700

    updated import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import "./App.css";
-import { fetchEndpoint } from "./decryptPath";
+import { fetchAllEndpoints } from "./decryptPath";
 
 function App() {
   const [levels, setLevels] = useState<any[]>([]);
